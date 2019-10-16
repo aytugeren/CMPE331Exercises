a5b3aabc68787450dfb67c0313c435e9b45800a0
Aytug Eren <aytgeren@gmail.com>
2019-10-16 19:00:44 +0300

    Add files via upload

---
+++ WaterSensorCode.ino
@@ -0,0 +1,53 @@
+int nemdeger = 0;
+int sensor = A0;
+int kirmiziled = 7;
+int yesilled = 5;
+
+void setup() {
+ pinMode(sensor, INPUT);
+ pinMode(kirmiziled, OUTPUT);
+ pinMode(yesilled, OUTPUT);
+ Serial.begin(9600);
+ Serial.println("Nem Durumu:");
+ Serial.println("-------------");
+}
+void loop() {
+  nemdeger = analogRead(A0);
+
+
+if(nemdeger > 901){
+Serial.print("Nem:");
+Serial.println(nemdeger);
+Serial.println("Durum: Toprak Kuru");
+Serial.println("");
+digitalWrite(kirmiziled, HIGH);
+delay(500);
+} else{
+  digitalWrite(kirmiziled, LOW);
+  delay(500);
+}
+  
+if(nemdeger > 251 && nemdeger <= 900){
+Serial.print("Nem:");
+Serial.println(nemdeger);
+Serial.println("Durum: Toprak Nemli");
+Serial.println("");
+digitalWrite(yesilled, HIGH);
+delay(500);
+} else{
+  digitalWrite(yesilled, LOW);
+  delay(500);
+}
+  
+if(250 > nemdeger){
+Serial.print("Nem:");
+Serial.println(nemdeger);
+Serial.println("Durum: Toprak Yas");
+Serial.println("");
+digitalWrite(kirmiziled, HIGH);
+delay(500);
+} else{
+  digitalWrite(kirmiziled, LOW);
+ delay(500);
+}
+}
